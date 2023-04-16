e6735ce9335cdb9d9c16383618363ace78c8d9bf
Hervé K <hkoueke@gmail.com>
2023-04-16 10:42:46 +0100

    Update DevicesWithAlertsSpecification.cs

--- src/SmartAc.Application/Specifications/Devices/DevicesWithAlertsSpecification.cs
+++ src/SmartAc.Application/Specifications/Devices/DevicesWithAlertsSpecification.cs
@@ -14,28 +14,28 @@ namespace SmartAc.Application.Specifications.Devices
         public DevicesWithAlertsSpecification(string serialNumber)
             : base(x => x.SerialNumber == serialNumber)
         {
-            AddInclude(x => x.Alerts);
+            AddInclude(x => x.Alerts.OrderBy(a => a.ReportedDateTime));
             AddInclude(x => x.DeviceReadings);
         }
 
         public DevicesWithAlertsSpecification(string serialNumber, int skip, int take)
             : base(x => x.SerialNumber == serialNumber)
         {
-            AddInclude(x => x.Alerts.OrderBy(a => a.AlertId).Skip(skip).Take(take));
+            AddInclude(x => x.Alerts.OrderBy(a => a.ReportedDateTime).Skip(skip).Take(take));
             AddInclude(x => x.DeviceReadings);
         }
 
         public DevicesWithAlertsSpecification(string serialNumber, AlertState alertState)
             : base(x => x.SerialNumber == serialNumber)
         {
-            AddInclude(x => x.Alerts.Where(a => a.AlertState == alertState));
+            AddInclude(x => x.Alerts.Where(a => a.AlertState == alertState).OrderBy(a => a.ReportedDateTime));
             AddInclude(x => x.DeviceReadings);
         }
 
         public DevicesWithAlertsSpecification(string serialNumber, AlertState alertState, int skip, int take)
             : base(x => x.SerialNumber == serialNumber)
         {
-            AddInclude(x => x.Alerts.Where(a => a.AlertState == alertState).OrderBy(a => a.AlertId).Skip(skip).Take(take));
+            AddInclude(x => x.Alerts.Where(a => a.AlertState == alertState).OrderBy(a => a.ReportedDateTime).Skip(skip).Take(take));
             AddInclude(x => x.DeviceReadings);
         }
     }
